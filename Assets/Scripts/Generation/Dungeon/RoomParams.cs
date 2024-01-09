9a8df250055b00f6fec872589378b762986549f3
“Alexis” <75834156+Framby21@users.noreply.github.com>
2024-01-09 09:57:46 +0100

    need to fix randomRange

---
+++ Assets/Scripts/Generation/Dungeon/RoomParams.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct RoomParams
+{
+    public int m_numberOfDoors;
+
+    public bool m_isStart;
+    public bool m_isEnd;
+}
