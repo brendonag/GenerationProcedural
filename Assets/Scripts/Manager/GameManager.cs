68c290726e11cf1e71e7b4608e19271571bb4efc
brendonag <70527282+brendonag@users.noreply.github.com>
2024-01-12 12:05:22 +0100

    great

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -6,37 +6,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [Header("Seed")]
-    [Min(0)][SerializeField] private int m_seed;
+    [SerializeField] private bool m_RandomSeed = true;
+    [SerializeField] private int m_seed;
+    
 
     [Header("Level Manager")]
     [SerializeField] private LevelManager[] m_levels;
     public LevelManager[] Levels { get { return m_levels; } }
-    #region room list
-    [Header("One Door")]
-    public List<GameObject> m_Down;
-    public List<GameObject> m_Top;
-    public List<GameObject> m_Right;
-    public List<GameObject> m_Left;
 
-    [Header("Two Door")]
-    public List<GameObject> m_DownTop;
-    public List<GameObject> m_DownLeft;
-    public List<GameObject> m_DownRight;
-
-    public List<GameObject> m_TopLeft;
-    public List<GameObject> m_TopRight;
+    [Header("Room")]
+    public GameObject m_start;
+    public List<GameObject> m_Rooms;
+    public GameObject m_end;
     
-    public List<GameObject> m_LeftRight;
 
-    [Header("Tree Door")]
-    public List<GameObject> m_DownTopLeft;
-    public List<GameObject> m_DownLeftRight;
-    public List<GameObject> m_DownTopRight;
-    public List<GameObject> m_TopLeftRight;
 
-    [Header("Hub")]
-    public List<GameObject> m_Hub;
-    #endregion
     
     static public GameManager instance;
     [HideInInspector] public System.Random m_Random;
@@ -44,7 +28,12 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         if (instance == null) { instance = this; }
-        m_Random = new System.Random(m_seed);
+
+        if(m_RandomSeed)
+        { 
+            m_seed = Random.Range(int.MinValue,int.MaxValue);
+        }
+        m_Random = new System.Random(m_seed);        
     }
 
 }
--- Assets/Scripts/RunGenerateDungeon.cs
+++ Assets/Scripts/RunGenerateDungeon.cs
@@ -6,50 +6,150 @@ public class RunGenerateDungeon : MonoBehaviour
 {
     private Vector3 m_position = Vector3.zero;
     private int m_last = -1;
+
     private List<GameObject> m_Rooms = new List<GameObject>();
+    [SerializeField]public bool[,] m_map;
+
+    Vector3 m_roomLeft = Vector3.zero;
+    Vector3 m_roomRight = Vector3.zero;
+    Vector3 m_roomTop = Vector3.zero;
+    Vector3 m_roomBottom = Vector3.zero;
+
+    List<Vector3> m_roomsLeft;
+    List<Vector3> m_roomsRight;
+    List<Vector3> m_roomsTop;
+    List<Vector3> m_roomsBottom;
+
+    private void Awake()
+    {
+        m_map = new bool[GameManager.instance.Levels[0].Room * 2, GameManager.instance.Levels[0].Room * 2];
+    }
+
     void Start()
     {
-        m_Rooms.Add(Instantiate(GameManager.instance.m_Hub[0],m_position, Quaternion.identity));
-
-        for(int i = 1; i < GameManager.instance.Levels[0].Room; i++)
+        m_Rooms.Add(Instantiate(GameManager.instance.m_start,m_position, Quaternion.identity));
+        m_map[GameManager.instance.Levels[0].Room, GameManager.instance.Levels[0].Room] = true;
+        for(int i = 1; i < GameManager.instance.Levels[0].Room-1; i++)
         {
-            int l_random = 0;
-            bool l_spawn = false;
-            Vector3 l_position = m_position;
+            SpawnRoom(GameManager.instance.m_Rooms[GameManager.instance.m_Random.Next(0, GameManager.instance.m_Rooms.Count)]);
+        }
+
+        SpawnRoom(GameManager.instance.m_end);
+    }
+
+    private void SpawnRoom(GameObject p_room)
+    {
+        int l_random = 0;
+        bool l_spawn = false;
+        Vector3 l_position = m_position;
+
+
+
+        do
+        {
+            l_spawn = true;
             do
             {
-                l_spawn = true;
-                do
+                l_random = GameManager.instance.m_Random.Next(0, 4);
+            } while (l_random == m_last);
+
+            switch (l_random)
+            {
+                case 0: l_position += Vector3.right ; break;
+
+                case 1: l_position += Vector3.down ; break;
+
+                case 2: l_position += Vector3.left ; break;
+
+                case 3: l_position += Vector3.up ; break;
+            }
+
+            if( m_map[GameManager.instance.Levels[0].Room + (int)l_position.x , GameManager.instance.Levels[0].Room + (int)l_position.y] == true)
+            {
+                l_spawn = false;
+                l_position = m_position;
+            }
+        } while (!l_spawn);
+
+        m_last = (l_random + 2) % 4;
+        m_position = l_position;
+        m_Rooms.Add(Instantiate(p_room, m_position, Quaternion.identity));
+        m_map[GameManager.instance.Levels[0].Room + (int)l_position.x, GameManager.instance.Levels[0].Room + (int)l_position.y] = true;
+
+        switch(m_last)
+        {
+            case 2: 
+                if (m_position.x >= m_roomRight.x)
                 {
-                    l_random = GameManager.instance.m_Random.Next(0, 4);
-                } while (l_random == m_last);
+                    m_roomRight = m_position;
+                    m_roomsRight.Add(m_roomRight);
+                }                  
+                break;
 
-                switch (l_random)
+            case 3:
+                if (m_position.y <= m_roomBottom.y)
                 {
-                    case 0: l_position += Vector3.up; break;
+                    m_roomBottom = m_position; 
+                    m_roomsBottom.Add(m_roomBottom);
+                }                    
+                break;
 
-                    case 1: l_position += Vector3.down; break;
-
-                    case 2: l_position += Vector3.left; break;
-
-                    case 3: l_position += Vector3.right; break;
-                }
-
-                foreach (GameObject l_room in m_Rooms)
+            case 0:
+                if (m_position.x <= m_roomLeft.x)
                 {
-                    if(l_position == l_room.transform.position)
-                    {
-                        l_spawn = false;
-                        l_position = m_position;
-                    }                        
-                }
+                    m_roomLeft = m_position;
+                    m_roomsLeft.Add(m_roomLeft);
+                }             
+                break;
 
-            } while (!l_spawn);
-            
-            m_last = l_random;
-            m_position = l_position;
-            m_Rooms.Add(Instantiate(GameManager.instance.m_Hub[0], m_position, Quaternion.identity));
+            case 1:
+                if (m_position.y >= m_roomTop.y)
+                {
+                    m_roomTop = m_position;
+                    m_roomsTop.Add(m_roomTop);
+                }                   
+                break;
         }
+
+
+        bool l_top = false;
+        bool l_bottom = false;
+        bool l_left = false;
+        bool l_right = false;
+
+        if (m_map[GameManager.instance.Levels[0].Room + (int)m_position.x, GameManager.instance.Levels[0].Room + (int)m_position.y + 1] == true)
+        {
+            l_top = true;
+        }
+
+        if (m_map[GameManager.instance.Levels[0].Room + (int)m_position.x, GameManager.instance.Levels[0].Room + (int)m_position.y - 1] == true)
+        {
+            l_bottom = true;
+        }
+
+        if (m_map[GameManager.instance.Levels[0].Room + (int)m_position.x + 1, GameManager.instance.Levels[0].Room + (int)m_position.y ] == true)
+        {
+            l_right = true;
+        }
+
+        if (m_map[GameManager.instance.Levels[0].Room + (int)m_position.x - 1, GameManager.instance.Levels[0].Room + (int)m_position.y] == true)
+        {
+            l_left = true;
+        }
+
+        if (l_top && l_bottom && l_left && l_right)
+        {
+            m_last = -1;
+            switch(GameManager.instance.m_Random.Next(0,4))
+            {
+                case 0: m_position = m_roomTop; break;
+                case 1: m_position = m_roomBottom; break;
+                case 2: m_position = m_roomLeft; break;
+                case 3: m_position = m_roomRight; break;
+            }               
+        }
+
+  
     }
 
 }
